b396d5683c4401500699fd1c62260dba68ab7b2a
Navp788 <64306161+Navp788@users.noreply.github.com>
2020-04-25 14:47:31 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,19 @@
+calculation= input('''FOR ADDITION PRESS : + ,FOR SUBSTRACT PRESS : - ,FOR DIVISION PRESS : /  ,FOR MULPICATION PRESS : * ,FOR MODULES PRESS : %
+PRESS HERE  :::''')
+
+x=float(input("ENTER YOUR FIRST NUMBER : " ))
+y=float(input("ENTER YOUR SECOND  NUMBER : " ))
+if calculation=='+':
+    print("ADDITION OF TWO NUMBERS IS :",x+y)
+elif calculation=='*':
+    print("MULTIPLICATION OF TWO NUMBERS IS :",x*y)
+elif calculation=='%':
+    print("MODULUS OF TWO NUMBERS IS :",x%y)
+elif calculation=='-':
+    print("SUBTRACTION OF TWO NUMBERS IS :",x-y)
+elif calculation=='/':
+    print("DIVISION OF TWO NUMBERS IS :",x/y)
+else:
+    print('PLEASE CHECK !')
+    print ("ENTER VALID INPUT")
+    
